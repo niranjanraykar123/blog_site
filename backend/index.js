2d1b065ee54efc90006a445d4c0f941db286d96b
raykarniranjan@gmail.com <raykarniranjan@gmail.com>
2024-05-25 11:56:52 +0530

    mongo db setup

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const connectDB = require('./config/db')
 const app = express()
 const port = 3000
-
+connectDB();
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
