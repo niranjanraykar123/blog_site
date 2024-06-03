58a1de1eff84b363a634b017ecbcc0ef66433e6b
niranjanraykar123 <raykarniranjan@gmail.com>
2024-06-03 20:41:47 +0530

    Added mongodb atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/blogdb");
+        await mongoose.connect("mongodb+srv://niranjanraykar:Niranjan2021@cluster0.gxve0sq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log('MongoDB connected');
     } catch (err) {
         console.error(err.message);
