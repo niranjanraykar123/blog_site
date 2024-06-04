14ec57d2c73fe2dc16eaf4d252d986b7f68fd099
niranjanraykar123 <raykarniranjan@gmail.com>
2024-06-04 13:51:40 +0530

    Update Navbar.jsx

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -55,9 +55,9 @@ const Navbar = ({ user, setUser }) => {
                 </div>
                 <div className="">
                     <Link to={'/posts'}>
-                        <button type="button" onClick={logout} className=" mr-4 focus:outline-none text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">POSTS</button>
+                        <button type="button"  className=" mr-4 focus:outline-none text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">POSTS</button>
                     </Link>
-                    {user && <Link to={'/'}>
+                    {user && <Link to={'/posts'}>
                         <button type="button" onClick={logout} className=" mr-4 focus:outline-none text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Log Out</button>
                     </Link>}
                     {!user && <Link to={'/'}>
