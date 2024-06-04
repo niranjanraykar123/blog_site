d1c2a38f894cf1a19c9f473ead8674084ca8cd93
niranjanraykar123 <raykarniranjan@gmail.com>
2024-06-04 09:48:20 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://niranjanraykar:Niranjan2021@cluster0.gxve0sq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("mongodb+srv://nickoberoy629:Niranjan123@cluster0.jeubyyf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log('MongoDB connected');
     } catch (err) {
         console.error(err.message);
